0fa64b4d3055419c62edf9212a9ab74059bd90f1
Ricardo <ricardo_cabista@hotmail.com>
2025-09-13 13:34:53 -0300

    chore: add Jest setup for testing

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(.*\\.mjs$|@testing-library|@babel))",
+  ],
+};
+
+export default createJestConfig(customJestConfig);
